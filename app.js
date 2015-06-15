7aef685889fc764a6f045a5a2af0704a723e3a71
Florian Besser <f.besser@hotmail.com>
2015-06-15 12:51:38 +0200

    Improved naming, now always leak stack traces to user

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var movie = require('./routes/movie');
+var indexRoute = require('./routes/index');
+var movieRoute = require('./routes/movie');
 
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
@@ -33,8 +33,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/movie', movie);
+app.use('/', indexRoute);
+app.use('/movie', movieRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,25 +45,11 @@ app.use(function(req, res, next) {
 
 // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: err
   });
 });
 
